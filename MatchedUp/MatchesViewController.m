2577fa808ecc5afe8190016f6e828513b8f45aee
Tom Pullen <tpullen21@gmail.com>
2015-01-27 22:11:32 +0000

    Started Chat Setup

--- MatchedUp/MatchesViewController.m
+++ MatchedUp/MatchesViewController.m
@@ -8,22 +8,85 @@
 
 #import "MatchesViewController.h"
 
-@interface MatchesViewController ()
+@interface MatchesViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *availableChatRooms;
 
 @end
 
 @implementation MatchesViewController
 
+#pragma mark - Lazy Instantiation
+
+-(NSMutableArray *)availableChatRoom {
+    if (!_availableChatRooms) {
+        _availableChatRooms = [[NSMutableArray alloc] init];
+    }
+    return _availableChatRooms;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self updateAvailableChatRooms];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Helper Methods
+
+- (void)updateAvailableChatRooms {
+    PFQuery *query = [PFQuery queryWithClassName:@"ChatRoom"];
+    [query whereKey:@"user1" equalTo:[PFUser currentUser]];
+    PFQuery *queryInverse = [PFQuery queryWithClassName:@"ChatRoom"];
+    [query whereKey:@"user2" equalTo:[PFUser currentUser]];
+    PFQuery *queryCombined = [PFQuery orQueryWithSubqueries:@[query, queryInverse]];
+    [queryCombined includeKey:@"chat"];
+    [queryCombined includeKey:@"user1"];
+    [queryCombined includeKey:@"user2"];
+    [queryCombined findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (!error) {
+            [self.availableChatRooms removeAllObjects];
+            self.availableChatRooms = [objects mutableCopy];
+            [self.tableView reloadData];
+        }
+    }];
+}
+
+#pragma mark - UITableView DataSource
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.availableChatRooms count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    PFObject *chatRoom = self.availableChatRooms[indexPath.row];
+    
+    PFUser *likedUser;
+    PFUser *currentUser = [PFUser currentUser];
+    PFUser *testUser1 = chatRoom[@"user1"];
+    
+    if ([testUser1.objectId isEqual:currentUser.objectId]) {
+        likedUser = [chatRoom objectForKey:@"user2"];
+    }
+    else {
+        likedUser = [chatRoom objectForKey:@"user1"];
+    }
+    
+    cell.textLabel.text = likedUser[@"profile"][@"firstName"];
+    
+    return cell;
 }
 
 /*
